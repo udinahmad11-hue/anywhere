7e537c67a5e328e0830da0ebca7a0b6c64fc9646
udinahmad11-hue <udinahmad11@gmail.com>
2026-06-26 20:30:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,10 @@ const cors_proxy = require("cors-anywhere");
 const host = "0.0.0.0";
 const port = process.env.PORT || 8000;
 
-cors_proxy
-  .createServer({
-    originWhitelist: [],
-    requireHeader: [],
-    removeHeaders: [
-      "cookie",
-      "cookie2",
-      "x-forwarded-for",
-      "x-real-ip"
-    ],
-    setHeaders: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Methods": "*"
-    }
-  })
-  .listen(port, host, () => {
-    console.log(`CORS Anywhere running on ${host}:${port}`);
-  });
+cors_proxy.createServer({
+  originWhitelist: [],
+  requireHeader: [],
+  removeHeaders: ["cookie", "cookie2"]
+}).listen(port, host, () => {
+  console.log("Running on port", port);
+});
